4bfdd4d74bd0c7c710237e833a388a872ec1ebae
Tugrul ASLAN <tugrulaslan@gmail.com>
2019-04-18 23:57:01 +0200

    re-arranged variables and added given when and then bdd tags

--- RussianRoulette/src/test/java/com/tugrulaslan/repository/HostRepositoryTest.java
+++ RussianRoulette/src/test/java/com/tugrulaslan/repository/HostRepositoryTest.java
@@ -21,26 +21,38 @@ public class HostRepositoryTest {
     @Autowired
     private HostRepository repository;
 
-    private ArrayList<String> expectedHosts = new ArrayList<>(Arrays.asList("https://mirror.karneval.cz/pub/linux/fedora/epel/7/x86_64/",
-                                                                            "https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/",
-                                                                            "https://ftp-stud.hs-esslingen.de/pub/epel/7/x86_64/"));
-
     @Test
-    public void shouldFindPolishHostFromSecureEuropeanHostsRussianRoulette(){
+    public void shouldFindPolishHostFromSecureEuropeanHostsRussianRoulette() {
+        //given
+        final String expectedAddress = "https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/";
+
+        //when
         final List<Host> hosts = repository.findByAddressContaining("http");
+
+        //then
         assertThat(hosts).isNotNull();
-        assertThat(hosts.get(2).getAddress()).isEqualTo("https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/");
+        assertThat(hosts.get(2).getAddress()).isEqualTo(expectedAddress);
     }
 
     @Test
-    public void shouldFindPolishHostFromSecureEuropeanHostsNonRussianRoulette(){
-        final Host host = repository.findByAddress("https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/");
+    public void shouldFindPolishHostFromSecureEuropeanHostsNonRussianRoulette() {
+        //given
+        final String expectedAddress = "https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/";
+
+        //when
+        final Host host = repository.findByAddress(expectedAddress);
+
+        //then
         assertThat(host).isNotNull();
+        assertThat(host.getAddress()).isEqualTo(expectedAddress);
     }
 
     @Test
-    public void shouldFindAllSecureEuropeanHostsRussianRoulette(){
+    public void shouldFindAllSecureEuropeanHostsRussianRoulette() {
+        //given-when
         final List<Host> hosts = repository.findByAddressContaining("https");
+
+        //then
         assertThat(hosts).isNotNull();
         assertThat(hosts.get(0).getAddress()).isEqualTo("https://mirror.karneval.cz/pub/linux/fedora/epel/7/x86_64/");
         assertThat(hosts.get(1).getAddress()).isEqualTo("https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/");
@@ -48,8 +60,15 @@ public class HostRepositoryTest {
     }
 
     @Test
-    public void shouldFindAllSecureEuropeanHostsNonRussianRoulette(){
+    public void shouldFindAllSecureEuropeanHostsNonRussianRoulette() {
+        //given
+        ArrayList<String> expectedHosts = new ArrayList<>(Arrays.asList("https://mirror.karneval.cz/pub/linux/fedora/epel/7/x86_64/",
+                "https://ftp.icm.edu.pl/pub/Linux/fedora/linux/epel/7/x86_64/",
+                "https://ftp-stud.hs-esslingen.de/pub/epel/7/x86_64/"));
+        //when
         final List<Host> hosts = repository.findByAddressContaining("https");
+
+        //then
         final List<String> hostNames = hosts.stream().map(Host::getAddress).collect(Collectors.toList());
         assertThat(hosts).isNotNull();
         assertThat(hostNames).isNotNull();
